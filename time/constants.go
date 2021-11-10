e60c7abb06a19287f1622fae944c8432415f04e1
Washington Rodrigues <wrodrigues@esapies.com.br>
2021-11-10 19:01:04 -0300

    feat(black-friday): [GI-79] - Remoção constantes duplicadas

--- time/constants.go
+++ time/constants.go
@@ -5,9 +5,5 @@ const (
 	DateFormat             = "2006-01-02"
 	DateTimeFormat         = "2006-01-02 15:04:05"
 	YearMonthFormat        = "2006-01"
-
-	AmericaBuenosAires = "America/Buenos_Aires"
-	AmericaSantiago = "America/Santiago"
-	AmericaLaPaz = "America/La_Paz"
 )
 
