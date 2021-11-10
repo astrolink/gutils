5eaaa46c8d86a005b27d05b1e14bb79b67af3853
Washington Rodrigues <wrodrigues@esapies.com.br>
2021-11-10 18:59:32 -0300

    feat(black-friday): [GI-79] - Função data localizada por country code

---
+++ time/constants.go
@@ -0,0 +1,13 @@
+package time
+
+const (
+	DateTimeTimeZoneFormat = "2006-01-02T15:04:05.000Z"
+	DateFormat             = "2006-01-02"
+	DateTimeFormat         = "2006-01-02 15:04:05"
+	YearMonthFormat        = "2006-01"
+
+	AmericaBuenosAires = "America/Buenos_Aires"
+	AmericaSantiago = "America/Santiago"
+	AmericaLaPaz = "America/La_Paz"
+)
+
