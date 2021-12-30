5a27355dafc6559cfdb38e24354af9ae913c4ad9
Washington Rodrigues <wrodrigues@esapies.com.br>
2021-12-30 16:55:16 -0300

    feat(time): [IN-167] - Inclusão constante de formatação de data e hora com timezone

--- time/constants.go
+++ time/constants.go
@@ -1,9 +1,11 @@
 package time
 
 const (
-	DateTimeTimeZoneFormat = "2006-01-02T15:04:05.000Z"
-	DateFormat             = "2006-01-02"
-	DateTimeFormat         = "2006-01-02 15:04:05"
-	YearMonthFormat        = "2006-01"
+	DateTimeTimeZoneFormat       = "2006-01-02T15:04:05.000Z"
+	DateFormat                   = "2006-01-02"
+	DateTimeFormat               = "2006-01-02 15:04:05"
+	DateHourMinuteFormat         = "2006-01-02 15:04"
+	DateHourMinuteTimeZoneFormat = "2006-01-02T15:04"
+	YearMonthFormat              = "2006-01"
 )
 
