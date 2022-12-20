379739f6129c7c91f8342b1ebaaf43a7ce0c9d29
Phelipe Galiotti <pgaliotti@esapiens.com.br>
2022-12-20 10:33:00 -0300

    feat(handle-transaction): - implementando metodo responsavel por lidar com as transacoes abertas pelo connectionmanager

--- db/connectionManager.go
+++ db/connectionManager.go
@@ -8,29 +8,43 @@ type ConnectionManager struct {
 	db *Database
 }
 
-//New is responsible of creating a new instance of consultQuestionRepository for mysql database
+//NewMySqlConnectionManager inicia uma conexão com o banco de dados mysql instanciando um *ConnectionManager
 func NewMySqlConnectionManager(config Config) *ConnectionManager {
 	mysql := NewMySQL(config)
 	return &ConnectionManager{db: mysql}
 }
 
-//New is responsible of creating a new instance of consultQuestionRepository for postgres database
+//NewPgSqlConnectionManager inicia uma conexão com o banco de dados postgres instanciando um *ConnectionManager
 func NewPgSqlConnectionManager(config Config) *ConnectionManager {
 	pgsql := NewPgSQL(config)
 	return &ConnectionManager{db: pgsql}
 }
 
-// Close closes database connection
+// HandleTransaction gerencia o estado da conexão efetivando
+// o commit ou o rollback de uma transação de acordo com o erro recebido
+// 		tx *sql.Tx  é a transação que está sendo trabalhada pela conexão
+//		err *error passar ponteiro do erro para ser analisado
+func (c *ConnectionManager) HandleTransaction(tx *sql.Tx, err *error) error {
+	defer c.Close()
+
+	if *err != nil {
+		return tx.Rollback()
+	}
+
+	return tx.Commit()
+}
+
+// StartTransaction inicia uma transação com o banco de dados
 func (c *ConnectionManager) StartTransaction() (*sql.Tx, error) {
 	return c.db.StartTransaction()
 }
 
-// Close closes database connection
+// GetConnection retorna uma conexão pré-estabelecida com o banco de dados
 func (c *ConnectionManager) GetConnection() *sql.DB {
 	return c.db.Conn
 }
 
-// Close closes database connection
+// Close encerra a conexão com o banco de dados
 func (c *ConnectionManager) Close() {
 	c.db.Close()
 }
